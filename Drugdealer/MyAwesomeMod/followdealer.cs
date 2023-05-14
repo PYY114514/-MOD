5b609401ca18b83c803973054bfb46bd8d7b3659
PYY114514 <132048331+PYY114514@users.noreply.github.com>
2023-05-14 11:29:24 +0800

    Add files via upload

---
+++ Drugdealer/MyAwesomeMod/followdealer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLibsCore.Test
+{
+    public class followdealer : CustomTrait
+    {
+        [RLSetup]
+        public static void Setup()
+        {
+            RogueLibs.CreateCustomTrait<followdealer>()
+                .WithName(new CustomNameInfo("正在跟着毒贩"))
+                .WithDescription(new CustomNameInfo("正在跟着毒贩"))
+                .WithUnlock(new TraitUnlock { CharacterCreationCost = 0, IsAvailable = false, IsAddedToCC = false, IsAvailableInCC = false }
+                );
+
+        }
+
+        public override void OnAdded() { }
+        public override void OnRemoved() { }
+
+    }
+}
